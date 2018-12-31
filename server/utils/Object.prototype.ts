aae58e4b5c58df56daa871729c6cce538bbf845f
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-31 14:02:38 +0100

    fix(utils): remove excess code

--- server/utils/Object.prototype.ts
+++
@@ -1,8 +0,0 @@
-export const removeFalsy = (obj: Object): Object => {
-    return Object
-        .keys(obj)
-        .filter(prop => !!obj[prop])
-        .map(prop => ({
-            [prop]: obj[prop],
-        }))
-}
