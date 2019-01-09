87d83939e20acdf329070164cec74be29d328634
Maikel van Veen <maikeloo10@hotmail.com>
2019-01-09 18:48:36 +0100

    feat(server/songsTable): setup table to receive creators

--- server/db/setup/createSongsTable.ts
+++ server/db/setup/createSongsTable.ts
@@ -4,6 +4,7 @@ export const createSongsTable = `
         _id serial PRIMARY KEY,
         title character varying(150) COLLATE pg_catalog."default" NOT NULL,
         "composerShareId" integer REFERENCES "artistShare"(_id),
+        "creatorIds" integer[] REFERENCES "artistShare"(_id),
         "createdAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
     )
     WITH (
